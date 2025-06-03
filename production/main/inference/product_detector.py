8877811e75dc9ce4ff75c403b35f3278c58ed992
GuilhermeLopesBertacini <guilb2005@gmail.com>
2025-06-03 14:08:50 -0300

    working pipeline

--- production/main/inference/product_detector.py
+++ production/main/inference/product_detector.py
@@ -1,5 +1,5 @@
 import os
-import time
+from time import perf_counter, sleep
 from typing import List, Optional, Tuple, Union
 import numpy as np
 from ultralytics import YOLO
@@ -8,29 +8,27 @@ import main.utils.config as constants
 from main.utils.logger_class import LoggerClass
 
 class ProductDetector:
-    def __init__(self, model_name: str = "2025-04-09/best.pt") -> None:
-        self.model_path: str = os.path.join(constants.MODELS_DIR, model_name)
-        self.model: YOLO = YOLO(self.model_path)
+    def __init__(self, model_path: str = constants.PRODUCT_DETECTOR_MODEL_PATH) -> None:
+        self.model: YOLO = YOLO(model_path)
 
     def predict(
         self,
-        image_path: Optional[str] = None,
+        image: np.array,
         conf_threshold: float = 0.25,
         show: bool = False
     ) -> Tuple[Results, float]:
-        if image_path is None:
-            image_path = os.path.join(constants.ASSETS_DIR, 'images/agua_cristal.jpeg')
 
-        start_time = time.perf_counter()
+        start_time = perf_counter()
+
 
         results: List[Results] = self.model.predict(
-            source=image_path,
+            source=image,
             conf=conf_threshold,
             max_det=1,
-            # verbose=False,
-            # show=show
+            verbose=False,
+            show=show
         )
-        elapsed_time = time.perf_counter() - start_time
+        elapsed_time = perf_counter() - start_time
         return self._get_boxes(results[0]), elapsed_time
 
     def _get_boxes(self, result: Results) -> Union[np.ndarray, List]:
@@ -38,7 +36,7 @@ class ProductDetector:
             predictions = result.boxes
 
             if not hasattr(predictions, "data"):
-                LoggerClass.info("No predictions found.")
+                LoggerClass.debug("No predictions found.")
                 return []
 
             tensor_boxes: np.ndarray = predictions.data.cpu().numpy()
@@ -46,8 +44,12 @@ class ProductDetector:
             if tensor_boxes.ndim != 2 or tensor_boxes.shape[1] < 6 or tensor_boxes.shape[0] == 0:
                 LoggerClass.info("No valid boxes.")
                 return []
-            LoggerClass.debug("Confidence:", tensor_boxes[4])
-            return tensor_boxes[:4]
+
+            LoggerClass.debug("Box found with confidence(s): %s", tensor_boxes[:, 4])
+            print("Tensor boxes:", tensor_boxes)
+
+            # Return only [x1, y1, x2, y2] for each box
+            return tensor_boxes[:, :4]
 
         except Exception as e:
             LoggerClass.error(f"Error extracting boxes: {e}")
